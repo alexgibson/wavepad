d11ca7c79b477eccb2ef9c85b906ea50ccbb824c
Alex Gibson <a.gibson@me.com>
2015-01-28 19:42:25 +0000

    Add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var deploy = require('gulp-gh-pages');
+var jshint = require('gulp-jshint');
+
+var options = {
+    branch: 'gh-pages'
+};
+
+gulp.task('deploy', ['js:lint'], function () {
+    return gulp.src('./**/*')
+        .pipe(deploy(options));
+});
+
+gulp.task('js:lint', function() {
+    return gulp.src('./js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('default', function () {
+    watch('./js/*.js', function () {
+        gulp.start('js:lint');
+    });
+});
