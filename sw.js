75f9d81f432da8d918a08bc9ff37a805cbf0c988
Alex Gibson <agibson@mozilla.com>
2016-01-31 15:41:37 -0800

    Update 2016-01-31T23:41:33.972Z

---
+++ sw.js
@@ -0,0 +1,38 @@
+/* globals self, caches */
+
+var staticCacheName = 'wave-pd1-v1';
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(staticCacheName).then(function(cache) {
+            return cache.addAll([
+                '/',
+                'js/bundle.js',
+                'css/styles.css'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith('wave-pd1-') &&
+                        cacheName != staticCacheName;
+                    }).map(function(cacheName) {
+                        return caches.delete(cacheName);
+                    })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
